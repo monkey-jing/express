5492e5c46103e787831d2183b107d56729e7628d
monkey-jing <554761618@qq.com>
2017-12-15 18:33:50 +0800

    ...

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
 app.use(express.static(__dirname+'/public'))
+
+app.use(function(req,res,next){
+    res.locals.showTests=app.get('env')!=='production'&&req.query.test==='1';
+    next();
+})
 app.get('/',function(req,res){
     res.render('home')
 })
